65c66f548abdc1b61600ffa8eb80299d961892da
Dom <dom@tide.org>
2025-06-06 16:20:37 +1000

    web-app publish

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Create metrics instance
 var metrics = new PrometheusMetricsExporter();
 
+// Example route that increments a metric
 app.MapGet("/", () =>
 {
     metrics.IncrementCounter("myapp_requests_total", help: "Total number of processed requests");
-    return "Hello Secure World!";
+    return "Web app is running!";
 });
 
+// /metrics endpoint for Prometheus
 app.MapGet("/metrics", () =>
 {
     var result = metrics.GetMetrics();
